3437faf138c52ebc8e223a19328cecd10507a217
Ashley Teow & Rio <pair+ashleyteow.rnose512@devbootcamp.com>
2017-08-28 14:53:40 -0700

    Create sessions helper methods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+  def current_user
+    @user ||= User.find(session[:id]) if session[:id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login
+    session[:id] = @user.id
+  end
+
+  def logout
+    session[:id] = nil
+  end
 end
