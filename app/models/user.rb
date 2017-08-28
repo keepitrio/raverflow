ace9aba712b9677a97dcf6b25f6c7b8ec0a0bb75
Ashley Teow & Rio <pair+ashleyteow.rnose512@devbootcamp.com>
2017-08-28 15:42:20 -0700

    Implement error handling for user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :email, :username, presence: true, uniqueness: true
+
   has_many :questions, foreign_key: :questioner_id
   has_many :answers, foreign_key: :answerer_id
   has_many :comments, foreign_key: :commenter_id
