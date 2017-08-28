f8c8bb42800d7f4b53e82cd7df6beea1e3407fec
Ashley Teow & Rio <pair+ashleyteow.rnose512@devbootcamp.com>
2017-08-28 14:57:39 -0700

    Create authenticate method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:email])
+    @user = User.authenticate(params[:email], params[:password])
     if @user
-      if @user.password == params[:password]
-        login
-        redirect_to @question
-      else
-        @errors = ["Incorrect email/password"]
-        render 'new'
-      end
+      login
+      redirect_to @question
     else
-      render 'users/new'
+      @errors = ["Incorrect email/password"]
+      render 'new'
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
   has_many :answers, foreign_key: :answerer_id
   has_many :comments, foreign_key: :commenter_id
   has_many :votes, foreign_key: :voter_id
+
+  def self.authenticate(login_email, login_password)
+    @user = User.find_by(email: login_email)
+    if !@user
+      nil
+    elsif @user.password == login_password
+      @user
+    elsif @user.password != login_password
+      nil
+    end
+  end
 end
